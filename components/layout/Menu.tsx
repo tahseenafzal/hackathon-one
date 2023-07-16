91f42df7bc3c9cdde0d0b0069e8aa60bf3e7f9e8
Tehseen Afzal <abuashja@gmail.com>
2023-07-16 23:59:24 +0500

    Update Menu.tsx
    Update styles

--- components/layout/Menu.tsx
+++ components/layout/Menu.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Menu = () => {
   return (
-    <ul className="block lg:flex gap-x-12">
+    <ul className="flex flex-col gap-4 lg:flex-row lg:gap-x-12 text-center text-[16px]">
       <li>Female</li>
       <li>Male</li>
       <li>Kids</li>
