03e6991523fd38fca38fb633a5ec50fdbe988e4e
Tehseen Afzal <abuashja@gmail.com>
2023-07-16 23:59:39 +0500

    Update Searchbar.tsx
    Add rounded-lg

--- components/layout/Searchbar.tsx
+++ components/layout/Searchbar.tsx
@@ -3,7 +3,7 @@ import { FiSearch } from "react-icons/fi";
 
 const Searchbar = () => {
   return (
-    <div className="flex  items-center border border-gray-300 w-[321px] h-[27px] px-[5px] rounded-xl">
+    <div className="flex  items-center border border-gray-300 w-[321px] h-[27px] px-[5px] rounded-lg">
       <FiSearch size={16} className="shrink-0" />
       <input
         className="w-full"
