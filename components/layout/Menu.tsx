8bee484aae734e5e4716f5ee6f655561be778c5b
Tehseen Afzal <abuashja@gmail.com>
2023-07-16 16:42:45 +0500

    Create Menu.tsx

---
+++ components/layout/Menu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <ul className="block lg:flex gap-x-12">
+      <li>Female</li>
+      <li>Male</li>
+      <li>Kids</li>
+      <li>All Products</li>
+    </ul>
+  );
+};
+
+export default Menu;
