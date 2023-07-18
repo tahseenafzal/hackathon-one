aef819ae39bab6663bef6180bf0e55fd3a40ff16
Tehseen Afzal <abuashja@gmail.com>
2023-07-18 17:09:44 +0500

    Update page.tsx
    Render promotion component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Hero from "@/components/views/Hero";
+import Promotions from "@/components/views/Promotions";
 import Image from "next/image";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <main>
+      <Hero />
+      <Promotions />
+    </main>
+  );
 }
