c3106c33b7f00863fb675b138fbfeb5353be0f85
Tehseen Afzal <abuashja@gmail.com>
2023-07-18 17:11:09 +0500

    Update Hero.tsx
    Update margins

--- components/views/Hero.tsx
+++ components/views/Hero.tsx
@@ -9,7 +9,7 @@ import feature4 from "@/public/media/bands/Featured4.webp";
 
 const Hero = () => {
   return (
-    <div className="flex gap-16 justify-between flex-1 my-16 mx-32 relative">
+    <div className="flex gap-16 justify-between flex-1 my-16 mx-4 lg:my-16 lg:mx-32 relative">
       <div className="flex flex-col gap-10">
         <span className="flex justify-center items-center font-semibold w-[120px] h-[40px] text-[#0000ff] bg-[#e1edff] rounded-md">
           Sale 70%
@@ -24,16 +24,20 @@ const Hero = () => {
         <button className="w-[35%] text-[1rem] p-4 leading-[18px] font-semibold bg-[#212121] text-[#fff] flex justify-center items-center gap-2 ">
           <FiShoppingCart /> Start Shopping
         </button>
-        <div className="grid grid-cols-4 gap-4">
-          <Image src={feature1} alt="brand1" />
-          <Image src={feature2} alt="brand2" />
-          <Image src={feature3} alt="brand3" />
-          <Image src={feature4} alt="brand4" />
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          <Image className="w-[100px]" src={feature1} alt="brand1" />
+          <Image className="w-[100px]" src={feature2} alt="brand2" />
+          <Image className="w-[100px]" src={feature3} alt="brand3" />
+          <Image className="w-[100px]" src={feature4} alt="brand4" />
         </div>
       </div>
-      <div className="flex-1">
+      <div className="hidden lg:block flex-1">
         <div className=" bg-[#ffece3] rounded-full w-[600px] h-[600px]">
-          <Image src={heroImage} alt="Hero" className="absolute top-[-5%]" />
+          <Image
+            src={heroImage}
+            alt="Hero"
+            className="absolute top-[-5%] w-[650px] h-[650px]"
+          />
         </div>
       </div>
     </div>
