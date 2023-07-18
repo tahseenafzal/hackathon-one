b139ceef03775d145a5c55d78340aa57411328e8
Tehseen Afzal <abuashja@gmail.com>
2023-07-18 17:10:48 +0500

    Update Searchbar.tsx
    Update rounded css

--- components/layout/Searchbar.tsx
+++ components/layout/Searchbar.tsx
@@ -3,10 +3,10 @@ import { FiSearch } from "react-icons/fi";
 
 const Searchbar = () => {
   return (
-    <div className="flex  items-center border border-gray-300 w-[321px] h-[27px] px-[5px] rounded-lg">
+    <div className="flex  items-center border border-gray-300 w-[321px] h-[27px] px-[5px] rounded-md">
       <FiSearch size={16} className="shrink-0" />
       <input
-        className="w-full"
+        className="w-full h-[25px] text-xs p-[5px]"
         type="text"
         placeholder="What you looking for"
       />
